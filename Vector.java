f7407a0d136c8f676f4622faf21ef5b23ebe32e5
codewithav <arpitanamdev120@gmail.com>
2026-06-20 21:49:06 +0530

    java

---
+++ Vector.java
@@ -0,0 +1,13 @@
+// import java.util.Vector;
+
+// public class Vector{
+    
+//     public static void main(String[] args){
+        
+//         Vector<String> v = new Vector<>();
+//         v.add("A");
+//         v.add("B");
+//         v.add("C");
+//         System.out.println(v);
+//     }
+// }
