b829944385cc2b16b144c977c44cba0a99651c08
codewithav <arpitanamdev120@gmail.com>
2026-06-23 21:19:08 +0530

    java

---
+++ Questions/Integerornot.java
@@ -0,0 +1,11 @@
+package Questions;
+
+import java.util.Scanner;
+
+public class Integerornot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+        sc.close();
+    }
+}
