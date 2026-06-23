9798e3a94c14d43c82e2b2dd41c9409d3fd4a9b9
codewithav <arpitanamdev120@gmail.com>
2026-06-23 21:03:04 +0530

    java

---
+++ Questions/Sumofthree.java
@@ -0,0 +1,13 @@
+package Questions;
+
+public class Sumofthree {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 20;
+        int c = 50;
+        // second way
+        int sum = a + b + c;
+        System.out.println(sum);
+        System.out.println(a + b + c);
+    }
+}
